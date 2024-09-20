e0cba206c886144fd1f12f118d687733b75fbe73
Jovan Karmakka <jrkarmau@student.jyu.fi>
2024-09-20 11:54:32 +0300

    1.06 valmis

--- Part1/Project/index.js
+++ Part1/Project/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 app.get('/', (request, response) => {
   response.send('<h1>Project start page</h1>')
